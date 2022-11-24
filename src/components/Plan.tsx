ef4105aa593a9738e09693b8605cbbac022adeff
Sxyntheon <jakob_kannenberg@icloud.com>
2022-11-24 17:43:22 +0100

    feat: Table content is now useState array

--- src/components/Plan.tsx
+++ src/components/Plan.tsx
@@ -5,11 +5,23 @@ import "../styles/Plan.css"
 import { useState } from "preact/hooks";
 
 export default function Plan(): JSX.Element {
+    const [content, setContent] = useState([""])
+
+    const createContent = []
+
+    for(let i = 0; i < 50; i++) {
+        let newContent = "Test" + i;
+        createContent.push(newContent);
+    }
+
+    setContent(createContent);
+    
     const tableDivs = [];
 
     for(let index = 0; index < 50; index++) {
         let tableIndex: string = "table-index-" + index;
-        tableDivs.push(<div id={tableIndex}><p>Test</p></div>)
+        tableDivs.push(<div id={tableIndex}><p>{content[index]}</p></div>)
+        //console.log(content[index]);
     }
 
     return(
