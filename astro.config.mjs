14496935367e8e5e0092dcf24042e0f527b533be
Sxyntheon <jakob_kannenberg@icloud.com>
2022-11-25 16:39:55 +0100

    feat: fixed typo / optimized font size for mobile

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,8 @@ import preact from "@astrojs/preact";
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [preact()],
