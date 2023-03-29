e431848416dc4c47d2da2e6a966dd2ca44ddcd1e
Sxyntheon <jakob_kannenberg@icloud.com>
2023-03-29 19:40:46 +0200

    fix(scaling): adjust scaling on mobile

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -7,16 +7,25 @@ import { verifyPassword, registerAccount, loginAccount, fetchJSessionId, saveUnt
 
 export default function LoginForm(): JSX.Element  {
     const [activeButton, setActiveButton] = useState<number>(1);
+    const [buttonStyle1, setButtonStyle1] = useState({borderBottom: "2px solid #5974e2"})
+    const [buttonStyle2, setButtonStyle2] = useState({})
     const [password, setPassword] = useState(""), [username, setUsername] = useState(""), [untisPassword, setUntisPassword] = useState(""), [untisUsername, setUntisUsername] = useState("")
     const [untisBoxStyle, setUntiBoxStyle] = useState({})
-    const [notice, showPasswordNotice] = useState(<></>)
+    const [notice, showPasswordNotice] = useState(<p style={{opacity: "0"}}>A</p>)
+
     
     useEffect(() => {
         if(activeButton == 2) {
-            setUntiBoxStyle({display: "inline-block"})
+            setButtonStyle2({borderBottom: "2px solid #5974e2"})
+            setButtonStyle1({borderBottom: "none"})
+            showPasswordNotice(<p style={{opacity: "0"}}>A</p>)
+            setUntiBoxStyle({opacity: "100"})
         }
         else {
-            setUntiBoxStyle({display: "none"})
+            setButtonStyle1({borderBottom: "2px solid #5974e2"})
+            setButtonStyle2({borderBottom: "none"})
+            showPasswordNotice(<p style={{opacity: "0"}}>A</p>)
+            setUntiBoxStyle({opacity: "0"})
         }
     }, [activeButton])
 
@@ -31,7 +40,7 @@ export default function LoginForm(): JSX.Element  {
                 if(event.target[0].value && event.target[1].value) {
                     setUsername(event.target[0].value)
                     setPassword(event.target[1].value)
-                    showPasswordNotice(<></>)
+                    showPasswordNotice(<p style={{opacity: "0"}}>A</p>)
                     fetchJSessionId(localStorage.getItem("untis_username"), localStorage.getItem("untis_password")).then((result) => {
                         if(result.JSessionId && result.personId) {
                             document.cookie = `JSESSIONID=${result.JSessionId}; max-age=600; secure; samesite=strict`
@@ -45,7 +54,7 @@ export default function LoginForm(): JSX.Element  {
                         
                 }
                 else {
-                    showPasswordNotice(<p>Bitte fülle alle Felder aus</p>)
+                    showPasswordNotice(<p style={{opacity: "100"}}>Bitte fülle alle Felder aus</p>)
                 }
             }
             if(activeButton == 2) {
@@ -56,10 +65,10 @@ export default function LoginForm(): JSX.Element  {
                     setUntisUsername(event.target[2].value)
                     setUntisPassword(event.target[3].value)
                     if(!verifyPassword(event.target[1].value)) {
-                        showPasswordNotice(<p>Dein Passwort muss mindestens 8 Zeichen lang sein <br/> und Groß-, Kleinbuchstaben, Zahlen und Sonderzeichen bestehen</p>)
+                        showPasswordNotice(<p style={{opacity: "100"}}>Dein Passwort muss mindestens 8 Zeichen lang sein <br/> und aus Groß-, Kleinbuchstaben, Zahlen und Sonderzeichen bestehen</p>)
                     }
                     else {
-                        showPasswordNotice(<></>)
+                        showPasswordNotice(<p style={{opacity: "0"}}>A</p>)
                         saveUntisCredentials(event.target[2].value, event.target[3].value)
                         fetchJSessionId(event.target[2].value, event.target[3].value).then((result) => {
                             if(result.JSessionId && result.personId) {
@@ -67,14 +76,14 @@ export default function LoginForm(): JSX.Element  {
                                 registerAccount("Account", "1Passwort!", result.personId)
                             }
                             else {
-                                showPasswordNotice(<p>Deine Untiszugangsdaten sind nicht korrekt</p>)
+                                showPasswordNotice(<p style={{opacity: "100"}}>Deine Untiszugangsdaten sind nicht korrekt</p>)
                             }
                         })
                     }
                         
                 }
                 else {
-                    showPasswordNotice(<p>Bitte fülle alle Felder aus</p>)
+                    showPasswordNotice(<p style={{opacity: "100"}}>Bitte fülle alle Felder aus</p>)
                 }
             }
         }
@@ -89,26 +98,19 @@ export default function LoginForm(): JSX.Element  {
             </div>
             <div className="login-box-container">
                 <div className="login-switch">
-                    <button id="login" onClick={() => {handleButtonClick(1)}}>Anmelden</button>
-                    <button id="register" onClick={() => {handleButtonClick(2)}}>Registrieren</button>
-                    <div className="underline" style={{
-                        transform: activeButton === 1 ? 'translateX(3%)' : 'translateX(86%)',
-                        width: activeButton === 1 ? '18%' : '22%'
-                        }
-                        }>
-                    </div>
+                    <button id="login" style={buttonStyle1} onClick={() => {handleButtonClick(1)} }>Anmelden</button>
+                    <button id="register" style={buttonStyle2}onClick={() => {handleButtonClick(2)}}>Registrieren</button>
                 </div>
                 <form onSubmit={handleSubmit}>
                     <input type="username" placeholder="Nutzername" className="input-box" />
                     <input type="password" placeholder="Passwort" className="input-box" />
-                    <p className="untis-login untis-box" style={untisBoxStyle}>Gib hier deine Untis-Daten ein:</p>
-                    <input type="username" placeholder="Units-Nutzername" className="input-box untis-box" style={untisBoxStyle}/>
+                    <div className="password-notice">{notice}</div>
+                    <input id="untis-username" type="username" placeholder="Units-Nutzername" className="input-box untis-box" style={untisBoxStyle}/>
                     <input type="password" placeholder="Untis-Passwort" className="input-box untis-box" style={untisBoxStyle}/>
                     <div className="button-container">
-                        <input type="submit" id="submit-button" />
+                        <input type="submit" id="submit-button" value="Absenden"/>
                     </div>
                 </form>
-                <div className="password-notice">{notice}</div>
             </div>
             
         </div>
