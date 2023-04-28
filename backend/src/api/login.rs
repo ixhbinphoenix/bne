9927d52ba97768732a34cca3e1e1bb12d0f77f1a
ixhbinphoenix <47122082+ixhbinphoenix@users.noreply.github.com>
2023-04-28 23:23:07 +0200

    format(backend): Format backend code

--- backend/src/api/login.rs
+++ backend/src/api/login.rs
@@ -1,43 +1,45 @@
 use actix_identity::Identity;
-use actix_web::{web, Responder, Result, HttpRequest, HttpMessage};
-use argon2::{Argon2, PasswordVerifier, PasswordHash};
-use serde::{Deserialize, Serialize};
+use actix_web::{web, HttpMessage, HttpRequest, Responder, Result};
+use argon2::{Argon2, PasswordHash, PasswordVerifier};
 use log::error;
-
-use crate::{database::surrealdb_repo::SurrealDBRepo, models::user_model::{UserCRUD, User}, prelude::*};
+use serde::{Deserialize, Serialize};
 
 use super::response::Response;
+use crate::{
+    database::surrealdb_repo::SurrealDBRepo, models::user_model::{User, UserCRUD}, prelude::*
+};
 
 
 #[derive(Deserialize)]
 pub struct LoginData {
     username: String,
-    password: String
+    password: String,
 }
 
 #[derive(Serialize)]
 pub struct LoginResponse {
-    untis_cypher: String
+    untis_cypher: String,
 }
 
-pub async fn login_post(data: web::Json<LoginData>, db: web::Data<SurrealDBRepo>, req: HttpRequest, id: Option<Identity>) -> Result<impl Responder> {
+pub async fn login_post(
+    data: web::Json<LoginData>, db: web::Data<SurrealDBRepo>, req: HttpRequest, id: Option<Identity>,
+) -> Result<impl Responder> {
     if id.is_some() {
-        return Ok(web::Json(Response::new_error(403, "Already logged in! Log out first".to_owned())))
+        return Ok(web::Json(Response::new_error(403, "Already logged in! Log out first".to_owned())));
     }
     let db_user: User = match UserCRUD::get_from_username(db, &data.username).await {
         Ok(n) => n,
-        Err(e) => {
-            match e {
-                Error::ObjectNotFound(_) => {
-                    return Ok(Response::new_error(403, "Username or Password is incorrect!".to_owned()).into())
-                }
-                _ => {
-                    error!("Unknown error occured when trying to get user.\n{}", e);
-                    return Ok(Response::new_error(500, "Internal Server Error".to_owned()).into())
-                }
+        Err(e) => match e {
+            Error::ObjectNotFound(_) => {
+                return Ok(Response::new_error(403, "Username or Password is incorrect!".to_owned()).into())
+            }
+            _ => {
+                error!("Unknown error occured when trying to get user.\n{}", e);
+                return Ok(Response::new_error(500, "Internal Server Error".to_owned()).into());
             }
         },
-    }.try_into()?;
+    }
+    .try_into()?;
 
     let argon2 = Argon2::default();
 
@@ -45,26 +47,21 @@ pub async fn login_post(data: web::Json<LoginData>, db: web::Data<SurrealDBRepo>
         Ok(hash) => hash,
         Err(_) => {
             error!("Error: Stored hash is not a valid hash. User: {}", db_user.username);
-            return Ok(Response::new_error(500, "Internal Server Error".to_owned()).into())
-        },
+            return Ok(Response::new_error(500, "Internal Server Error".to_owned()).into());
+        }
     };
 
     match argon2.verify_password(data.password.as_str().as_bytes(), &db_hash) {
-        Ok(_) => {
-            match Identity::login(&req.extensions(), db_user.id.expect("id to exist after conversion check")) {
-                Ok(_) => {
-                    Ok(Response::<LoginResponse>::new_success(LoginResponse {
-                        untis_cypher: db_user.untis_cypher.clone()
-                    }).into())
-                },
-                Err(e) => {
-                    error!("Error: Unknown error trying to login to Identity\n{}", e);
-                    Ok(Response::new_error(500, "Internal Server Error".to_owned()).into())
-                },
+        Ok(_) => match Identity::login(&req.extensions(), db_user.id.expect("id to exist after conversion check")) {
+            Ok(_) => Ok(Response::<LoginResponse>::new_success(LoginResponse {
+                untis_cypher: db_user.untis_cypher.clone(),
+            })
+            .into()),
+            Err(e) => {
+                error!("Error: Unknown error trying to login to Identity\n{}", e);
+                Ok(Response::new_error(500, "Internal Server Error".to_owned()).into())
             }
         },
-        Err(_) => {
-            Ok(Response::new_error(403, "Username or Password is incorrect!".to_owned()).into())
-        },
+        Err(_) => Ok(Response::new_error(403, "Username or Password is incorrect!".to_owned()).into()),
     }
 }
