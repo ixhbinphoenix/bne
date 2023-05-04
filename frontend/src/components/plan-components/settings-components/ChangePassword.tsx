8f1580a7fd9d0f7a034d7e0a7d32fcfb3b7791a0
Sxyntheon <jakob_kannenberg@icloud.com>
2023-05-04 21:16:08 +0200

    feat: add provisionally js method

--- frontend/src/components/plan-components/settings-components/ChangePassword.tsx
+++ frontend/src/components/plan-components/settings-components/ChangePassword.tsx
@@ -1,27 +1,39 @@
 /* @jsxImportSource preact */
 
-import "../../../styles/SettingsElement.scss"
+import "../../../styles/SettingsElement.scss";
 import type { JSX } from "preact";
+import { changePassword } from "../../../api/theBackend";
 
 export default function ChangePassword(): JSX.Element {
-    return (
-      <div class="new-password-content">
-        <div class="form-container">
-            <h2>Such dir ein neues Passwort aus</h2>
-            <form action="http://localhost:8089" autocomplete="on" method="post" target="dummyframe">
-            <input name="current_pwd" type="password" placeholder="Aktuelles Passwort" autocomplete="current-password" />
-              <input
-                name="new_pwd"
-                type="password"
-                placeholder="Neues Passwort"
-                autocomplete="new-password"
-                pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{8,}$"
-                title="Dein Passwort muss mindestens als 8 Zeichen lang sein und eine Zahl, ein Sonderzeichen, einen Klein- und einen Großbuchstaben enthalten"
-              />
-              <input type="submit" id="submit-button" />
-          </form>
-          <iframe name="dummyframe" id="dummyframe" style="display: none"></iframe>
-        </div>
+  const sendPasswordChange = (event: any) => {
+    event.preventDefault();
+    changePassword(event.target[0].value, event.target[1].value);
+  };
+  return (
+    <div class="new-password-content">
+      <div class="form-container">
+        <h2>Such dir ein neues Passwort aus</h2>
+        <form onSubmit={sendPasswordChange} autocomplete="on">
+          <input
+            name="current_pwd"
+            type="password"
+            placeholder="Aktuelles Passwort"
+            autocomplete="current-password"
+            required
+          />
+          <input
+            name="new_pwd"
+            type="password"
+            placeholder="Neues Passwort"
+            autocomplete="new-password"
+            pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z\d]).{8,}$"
+            title="Dein Passwort muss mindestens als 8 Zeichen lang sein und eine Zahl, ein Sonderzeichen, einen Klein- und einen Großbuchstaben enthalten"
+            required
+          />
+          <input type="submit" id="submit-button" />
+        </form>
+        <iframe name="dummyframe" id="dummyframe" style="display: none"></iframe>
       </div>
-    );
+    </div>
+  );
 }
