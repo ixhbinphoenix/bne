327bef2717a0f323858fb79d4a2202201aa09305
Sxyntheon <jakob_kannenberg@icloud.com>
2023-08-07 16:18:01 +0200

    feat(caching): extend caching rules

--- frontend/src/pwa/cachingStrategies.ts
+++ frontend/src/pwa/cachingStrategies.ts
@@ -1,15 +1,25 @@
 class Strategies {
-  public static cacheName = "v1";
-
-  public static async CacheOnly(event: FetchEvent): Promise<Response | undefined> {
-    return caches.match(event.request);
+  static getTimeStampStatus(name: string, maxAgeSeconds: number = 2630000): boolean {
+    const cookie = document.cookie.match("(^|;)\\s*" + name + "\\s*=\\s*([^;]+)")?.pop() || "";
+    if (!cookie) {
+      document.cookie = `${name}; max-age=${maxAgeSeconds}; secure; samesite=strict`;
+      return false;
+    }
+    return true;
   }
-  public static async CacheFirst(event: FetchEvent): Promise<Response> {
+  static async clearCache(cacheName: string): Promise<boolean> {
+    return await caches.delete(cacheName);
+  }
+  public static async CacheOnly(event: FetchEvent): Promise<Response> {
+    return (await caches.match(event.request)) || this.ErrorMessage();
+  }
+  public static async CacheFirst(event: FetchEvent, cacheName: string = "generic"): Promise<Response> {
+    //const notExpired = this.getTimeStampStatus(cacheName);
     const response = await caches.match(event.request);
     return (
       response ||
       fetch(event.request).then(async (response): Promise<Response> => {
-        const cache = await caches.open(Strategies.cacheName);
+        const cache = await caches.open(cacheName);
         cache.put(event.request, response.clone());
         return response;
       })
@@ -18,46 +28,44 @@ class Strategies {
   public static async NetworkOnly(event: FetchEvent): Promise<Response> {
     return fetch(event.request);
   }
-  public static async NetworkFirst(event: FetchEvent): Promise<Response | undefined> {
-    return fetch(event.request).then((response: Response) => {
+  public static async NetworkFirst(event: FetchEvent): Promise<Response> {
+    return fetch(event.request).then(async (response: Response) => {
       if (response.ok) {
         return response;
       } else {
-        return caches.match(event.request);
+        return (await caches.match(event.request)) || this.ErrorMessage();
       }
     });
   }
-  public static async StaleWhileRevalidate(event: FetchEvent): Promise<Response> {
+  public static async StaleWhileRevalidate(event: FetchEvent, cacheName: string = "generic"): Promise<Response> {
     return caches.match(event.request).then((cacheResponse: Response | undefined) => {
       let fetchResponse = fetch(event.request).then(async (response): Promise<Response> => {
-        const cache = await caches.open(Strategies.cacheName);
+        const cache = await caches.open(cacheName);
         cache.put(event.request, response.clone());
         return response;
       });
-      return cacheResponse || fetchResponse;
+      return cacheResponse || fetchResponse || this.ErrorMessage();
     });
   }
-  public static async NetworkRevalidateAndCache(event: FetchEvent): Promise<Response> {
+  public static async NetworkRevalidateAndCache(event: FetchEvent, cacheName: string = "generic"): Promise<Response> {
     return fetch(event.request).then(
       async (fetchResponse: Response): Promise<Response> => {
         if (fetchResponse.ok) {
-          const cache = await caches.open(Strategies.cacheName);
+          const cache = await caches.open(cacheName);
           cache.put(event.request, fetchResponse.clone());
           return fetchResponse;
         }
         const cacheResponse = await caches.match(event.request);
-        return (
-          cacheResponse ||
-          new Response(JSON.stringify({ success: false, message: "Connection offline" }), { status: 404 })
-        );
+        return cacheResponse || this.ErrorMessage();
       },
       async () => {
         const cacheResponse = await caches.match(event.request);
-        return (
-          cacheResponse ||
-          new Response(JSON.stringify({ success: false, message: "Connection offline" }), { status: 404 })
-        );
+        return cacheResponse || this.ErrorMessage();
       }
     );
+  }
+  public static async ErrorMessage(): Promise<Response> {
+    console.log("request was not in cache");
+    return new Response(JSON.stringify({ success: false, message: "Connection offline" }), { status: 404 });
   }
 }
--- frontend/src/pwa/serviceWorker.ts
+++ frontend/src/pwa/serviceWorker.ts
@@ -2,8 +2,7 @@
  * @author Sxyntheon
  *
  * @description Service Worker for enabling offline experience with TheSchedule
- **/
-
+**/
 importScripts("./cachingStrategies.ts");
 
 const cache: string = "v1";
@@ -11,24 +10,54 @@ const cache: string = "v1";
 async function cacheFonts(event: FetchEvent): Promise<Response> {
   return Strategies.CacheFirst(event);
 }
-async function cacheStyles(event: FetchEvent) {
+async function cacheStyles(event: FetchEvent): Promise<Response> {
   return Strategies.StaleWhileRevalidate(event);
 }
-
+async function cacheScripts(event: FetchEvent): Promise<Response> {
+  return Strategies.StaleWhileRevalidate(event);
+}
+async function cacheComponents(event: FetchEvent): Promise<Response> {
+  return Strategies.StaleWhileRevalidate(event)
+}
 async function handleFetch(event: FetchEvent): Promise<Response> {
   const url = event.request.url;
   const fileExtension = url.match(/\.[0-9a-z]+$/i);
+  const blacklist = "https://localhost:8080";
+  if (url.startsWith(blacklist)) {
+    console.log(url, " is blacklisted")
+    return Strategies.NetworkOnly(event)
+  }
   if (!fileExtension) {
     return Strategies.NetworkRevalidateAndCache(event);
   } else {
     switch (fileExtension[0]) {
       case ".woff":
+        console.log(url)
+        console.count("Font from Cache")
         return cacheFonts(event);
       case ".woff2":
+        console.log(url);
+        console.count("Font from Cache");
         return cacheFonts(event);
       case ".css":
+        console.log(url);
+        console.count("css from cache")
         return cacheStyles(event);
+      case ".scss":
+        console.log(url);
+        console.count("css from cache")
+        return cacheStyles(event)
+      case ".ts":
+        console.log(url);
+        console.count("script from cache")
+        return cacheScripts(event)
+      case ".tsx":
+        console.log(url);
+        console.count("component from cache")
+        return cacheComponents(event)
       default:
+        console.log(url);
+        console.count("default caching")
         return Strategies.NetworkRevalidateAndCache(event);
     }
   }
