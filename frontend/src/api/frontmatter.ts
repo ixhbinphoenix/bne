cd0a3a06e6a157dc5cf6ba85a496a7205bcbdac9
ixhbinphoenix <phoenix@ixhby.dev>
2024-03-19 15:00:02 +0100

    fix: Logiin and sessions

--- frontend/src/api/frontmatter.ts
+++ frontend/src/api/frontmatter.ts
@@ -3,7 +3,7 @@ class Request {
 
   public static async Post(path: string, data?: object): Promise<any> {
     try {
-      let result = await fetch("https://api.theschedule.de/" + path, {
+      let result = await fetch("https://172.17.0.1:8080/" + path, {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
@@ -26,7 +26,7 @@ class Request {
   }
   public static async Get(path: string, headers?: HeadersInit): Promise<any> {
     try {
-      let result = await fetch("https://api.theschedule.de/" + path, {
+      let result = await fetch("https://172.17.0.1:8080/" + path, {
         headers,
         method: "GET",
         credentials: "include"
