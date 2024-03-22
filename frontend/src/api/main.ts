1e61c27c1ac55d86eeccf2161facea0fea49bc02
ixhbinphoenix <phoenix@ixhby.dev>
2024-03-22 17:42:24 +0100

    fix: API Paths :3

--- frontend/src/api/main.ts
+++ frontend/src/api/main.ts
@@ -65,7 +65,7 @@ export const SubjectNames: { [key: string]: string } = {
   LI: "Literatur"
 };
 export function JSESSIONIDCookieString(JSESSIONID: string): string {
-  return `JSESSIONID=${JSESSIONID}; max-age=600; secure; samesite=none; domain=localhost`;
+  return `JSESSIONID=${JSESSIONID}; max-age=600; secure; samesite=none; domain=.theschedule.de`;
 }
 export async function getCommitHash(): Promise<string> {
   const result = await fetch("https://api.github.com/repos/ixhbinphoenix/bne/commits/master", {
